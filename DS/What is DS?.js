329da0bd8c8c5372679f46061911762e2e74a026
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-07-19 14:01:23 -0500

    array data structures

---
+++ DS/What is DS?.js
@@ -0,0 +1,43 @@
+// Data structures are a collection of values
+// Types of DS
+// Arrays
+// Stacks
+// Queues
+// Linked Lists
+// Trees
+// Tries
+// Graphs
+// Hash Tables
+
+// ---------------------------------->
+// ARRAYS
+// organizes items sequentially - one after another
+// good if all you need is to store some data 
+// and iterate over it
+
+const strings = ['a', 'b', 'c', 'd'];
+// 4*4 = 16 bytes of storage on a 32 bit system
+
+// push
+strings.push('e') // O(1)
+
+// pop
+strings.pop('e') // O(1)
+
+// unshift
+strings.unshift('x') // O(n)
+
+// splice
+strings.splice(2, 0, 'alien') // O(n)
+
+// Types of Arrays
+// Static & Dynamic
+
+// Stratic arrays are limiting since 
+// the number of items it can hold needs to 
+// be specified
+
+// Dynamic allows to copy and rebuild an array
+
+
+
