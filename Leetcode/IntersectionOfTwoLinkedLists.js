dd0a1a2819df5bd729155e96fd2d0bd5705e4120
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-09-20 11:25:38 -0500

    continue to practice linked lists

--- Leetcode/IntersectionOfTwoLinkedLists.js
+++ Leetcode/IntersectionOfTwoLinkedLists.js
@@ -63,3 +63,56 @@ const getIntersectionNode = (headA, headB) => {
 }
 
 getIntersectionNode(listA, listB)
+
+
+// Larger version
+
+const getIntersectionNode2 = (headA, headB) => {
+            
+    // find length of A
+    let lengthA = 0
+    let tempA = headA
+    while(tempA){
+        lengthA++
+        tempA = tempA.next
+    }
+
+    // find length of B
+    let lengthB = 0
+    let tempB = headB
+    while(tempB){
+        lengthB++
+        tempB = tempB.next
+    }
+
+    let diff = Math.abs(lengthA - lengthB)
+
+    tempA = headA
+    tempB = headB
+
+    if(lengthA > lengthB){
+        while(diff-- > 0){
+            tempA = tempA.next
+        }
+    }else{
+        while(diff-- > 0){
+            tempB = tempB.next
+        }
+    }
+
+
+    // Check for equality
+    while(tempA !== tempB){
+        tempA = tempA.next
+        tempB = tempB.next
+
+        if(tempA == null || tempB == null){
+            console.log(null)
+            return null
+        }
+    }
+
+    console.log(tempA)
+}
+
+getIntersectionNode2(listA, listB)
